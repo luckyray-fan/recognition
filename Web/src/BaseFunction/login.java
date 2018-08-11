53b886286e653634b4dbfa9c8654096a786e6c32
Yoshiro_fan <Yoshiro_fan@163.com>
2018-08-11 12:03:33 +0800

    测试

--- Web/src/BaseFunction/login.java
+++ Web/src/BaseFunction/login.java
@@ -9,11 +9,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+@WebServlet(name = "login",urlPatterns = "/login")
 public class login extends HttpServlet {
     private static final long serialVersionUID = 1L;
     Connection conn;
