895cbe612c3ff9aad23631e8a3630f47bd46fdbe
dell <1836662622@qq.com>
2018-08-12 17:11:05 +0800

    公司界面

---
+++ Web/src/BaseFunction/seemeeting.java
@@ -0,0 +1,31 @@
+package BaseFunction;
+import Tool.*;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class seemeeting extends HttpServlet{
+
+    public void service(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        HttpSession session=request.getSession();
+        user user =(user)session.getAttribute("user");
+        if(user.companyname==null)
+            response.sendRedirect("entercompany_interface.jsp");
+        else{
+            response.sendRedirect("meeting_interface.jsp");
+        }
+
+    }
+}
