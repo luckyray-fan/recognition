b02ba4906b59975f17c78dc248ea12f833cc9627
dell <1836662622@qq.com>
2018-08-12 16:21:33 +0800

    公司界面

--- Web/src/BaseFunction/login.java
+++ Web/src/BaseFunction/login.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.swing.*;
 
 public class login extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -44,13 +43,14 @@ public class login extends HttpServlet {
             String sql = "SELECT * FROM userinfo WHERE  account='" + account+ "' AND   pwd='" + pwd + "'"; // 查询用户信息的SQL语句,在参数前加上binary 就可以使查询结果区分大小写，因为MySQL默认不区分大小写
             ResultSet rs = stmt.executeQuery(sql); // 执行查询
             if (rs.next()) { // 有数据库中存在该用户
+                out.print("登陆成功");
                 user user=new user(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9));
                 HttpSession session=request.getSession();
                 session.setAttribute("user",user);
                 response.sendRedirect("function_interface.jsp");
             }
             else{
-                response.sendRedirect("Main_interface.jsp");
+                out.print("用户名不存在或者密码错误");
             }
         } catch (SQLException e) {
             e.printStackTrace();
