60756961a0e46b2d96fdaa8b48c308b5437c6e07
dell <1836662622@qq.com>
2018-08-13 19:15:55 +0800

    公司界面

---
+++ Web/src/Tool/meetinglist.java
@@ -0,0 +1,12 @@
+package Tool;
+
+public class meetinglist {
+    public String list1;
+    public String list2;
+    public String list3;
+    public meetinglist(String list1,String list2,String list3){
+        this.list1=list1;
+        this.list2=list2;
+        this.list3=list3;
+    }
+}
